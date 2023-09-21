7c6101005480df2e6a5d5fb58bcea084f9fd2c75
Dustin <dustin.naquin@gmail.com>
2023-09-20 22:50:21 -0500

    Created and implemented read_csv.py

---
+++ read_csv.py
@@ -0,0 +1,35 @@
+import csv
+
+
+CSVFILEPATH = "data_structures/csv_example.csv"
+
+
+def csvToList(fileName):
+    with open(fileName) as data:
+        csv_data = list(csv.reader(data))
+
+    return csv_data
+
+
+def writeToCsv(filename, inputList):
+    with open(filename, 'a', newline='\n') as data:
+        csv.writer(data).writerow(inputList)
+
+
+for item in csvToList(CSVFILEPATH):
+    if len(item) > 0:
+        print("Interface Name:", item[0])
+        print("Nework Name:", item[1])
+        print("IP Address:", item[2])
+        print("\n")
+
+
+answer = input("Add new Interface (y/n)?")
+
+if(answer == "y"):
+    intName = input("Interface name:")
+    netName = input("Network name:")
+    ipAdd = input("IP Address:")
+    input = [intName, netName, ipAdd]
+    writeToCsv(CSVFILEPATH, input)
+
