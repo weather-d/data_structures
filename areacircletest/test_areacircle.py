8f624d9f362db5947b8d4e2ea3b0e2a825565085
Dustin <dustin.naquin@gmail.com>
2023-09-24 22:52:44 -0500

    implemented areacircle and the test_areacircle

---
+++ areacircletest/test_areacircle.py
@@ -0,0 +1,17 @@
+from math import pi
+import unittest
+from areacircletest.areacircle import area_of_circle
+
+class TestAreaCircle(unittest.TestCase):
+
+    def test_area_of_circle(self):
+        self.assertAlmostEqual(area_of_circle(1), pi)
+        self.assertAlmostEqual(area_of_circle(0), 0)
+        self.assertAlmostEqual(area_of_circle(3.5), pi * 3.5**2)
+
+    def test_values(self):
+        self.assertRaises(ValueError, area_of_circle, -1)
+
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ read_yaml.py
@@ -0,0 +1,15 @@
+import yaml
+from pprint import pprint
+
+with open("data_structures/yaml_example.yaml") as data: 
+    yaml_dict = yaml.safe_load(data.read())
+newAddress = [{"ip": "172.16.0.5", "netmask": "255.255.255.0"}]
+
+yaml_dict["addresses"] += newAddress
+
+pprint(yaml.dump(yaml_dict))
+pprint(yaml.dump(yaml_dict, default_flow_style=False))
+pprint(yaml.dump(yaml_dict, default_flow_style=True))
+
+with open("data_structures/yaml_example.yaml", "w") as data: 
+    data.write(yaml.dump(yaml_dict, default_flow_style=False))
